086a741411028a24971ce69e81a24fa4293d9afd
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2024-04-05 17:16:18 +1300

    Add form submission

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amaando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+]
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New' });
+});
+
+router.post('/new', function(req, res, next) {
+  const {user, text } = req.body;
+  messages.push({ text: text, user: user, added: new Date() });
+  res.redirect('/');
+})
 
 module.exports = router;
